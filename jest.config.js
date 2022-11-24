700866dd82d0b838a4e0b51ae578457aa5ff07b3
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2022-11-24 05:53:45 -0800

    chore: 🔧 upgrade packages

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   coveragePathIgnorePatterns: ['/node_modules/'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json',
-      diagnostics: false,
-    },
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+        diagnostics: false,
+      },
+    ],
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   testMatch: ['**/*.(src|test|spec).(js|jsx|ts|tsx)'],
--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,17 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import resolve, { DEFAULTS as RESOLVE_DEFAULTS } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import typescript from 'rollup-plugin-typescript2'
-import { terser } from 'rollup-plugin-terser'
 import json from '@rollup/plugin-json'
-import sourceMaps from 'rollup-plugin-sourcemaps'
+import resolve, { DEFAULTS as RESOLVE_DEFAULTS } from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
+import terser from '@rollup/plugin-terser'
 import analyze from 'rollup-plugin-analyzer'
-import visualizer from 'rollup-plugin-visualizer'
-import postcss from 'rollup-plugin-postcss'
 import copy from 'rollup-plugin-copy'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import typescript from 'rollup-plugin-typescript2'
+import { visualizer } from 'rollup-plugin-visualizer'
+import postcss from 'rollup-plugin-postcss'
 
+/** @type {import('rollup').RollupOptions['output']} */
 const outputs = [
   {
     dir: 'dist/esm',
@@ -19,6 +20,7 @@ const outputs = [
     globals: {},
     sourcemap: true,
     exports: 'auto',
+    preserveModules: true,
   },
   {
     dir: 'dist/cjs',
@@ -27,9 +29,11 @@ const outputs = [
     globals: {},
     sourcemap: true,
     exports: 'auto',
+    preserveModules: true,
   },
 ]
 
+/** @type {import('rollup').RollupOptions} */
 export default {
   input: ['src/index.ts'],
   output: outputs,
@@ -109,7 +113,6 @@ export default {
       filename: 'rollup-plugin-visualizer-stats.html',
     }),
   ],
-  preserveModules: true,
   external: [
     // https://github.com/HarveyD/react-component-library/issues/19#issuecomment-652323218
     'tslib',
